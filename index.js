1318ab41c6abcbb86c5aa1b256eb4e8b478d0bd3
Amal <amalrajsr19@gmail.com>
2024-09-30 21:39:46 +0530

    Completed navbar session

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('nav-button').addEventListener('click', function() {
+  const navbar = document.getElementById('navbar-hamburger');
+  navbar.classList.toggle('hidden');
+  console.log('Button clicked, navbar toggled');
+});
+
+
