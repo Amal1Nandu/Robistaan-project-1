5d586736e0397bd665015156f8e8849ffb4b977c
Amal <amalrajsr19@gmail.com>
2024-10-03 19:37:45 +0530

    convert external css to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+            keyframes: {
+        animate: {
+          '0%': { transform: 'skewX(45deg) scaleY(0)' },
+          '100%': { transform: 'skewX(0deg) scaleY(1)' },
+        },
+      },
+      animation: {
+        animate: 'animate 800ms forwards ease',
+      },
+    },
+  },
+
+  plugins: [],
+};
+
