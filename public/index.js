1e7fc7423b25f3b6811e32753cfc0479404280cd
Amal <amalrajsr19@gmail.com>
2024-10-06 23:32:04 +0530

    complete carousel

--- public/index.js
+++ public/index.js
@@ -1,7 +1,6 @@
+// navbar
 document.getElementById('nav-button').addEventListener('click', function() {
   const navbar = document.getElementById('navbar-hamburger');
   navbar.classList.toggle('hidden');
  
 });
-
-
