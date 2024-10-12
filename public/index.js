bfc3453c066c3ce04c706bd72fdbecc2e805f8f5
Amal <amalrajsr19@gmail.com>
2024-10-12 22:53:50 +0530

    responsive correction

--- public/index.js
+++ public/index.js
@@ -9,6 +9,30 @@ document.getElementById('nav-button').addEventListener('click', function() {
   navbar.classList.toggle('hidden');
 });
 
+// carousel auto play
+
+document.addEventListener('DOMContentLoaded', function() {
+  let currentSlide = 0;
+  const slides = document.querySelectorAll('.carousel-item');
+  const totalSlides = slides.length;
+  
+  const showSlide = (index) => {
+    slides.forEach((slide, i) => {
+      slide.style.display = i === index ? 'block' : 'none';
+    });
+  };
+
+  const nextSlide = () => {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+  };
+  
+  showSlide(currentSlide); // Initialize with the first slide
+  setInterval(nextSlide, 3000); // Change slide every 3 seconds
+});
+
+
+
 //text animation
 document.addEventListener('DOMContentLoaded', () => {
 let mySplitText = new SplitType(".split", {type: "chars"});
