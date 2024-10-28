59bb9ae6bd9c48a9c91f4dd44a483f283f0d12d2
Amal <amalrajsr19@gmail.com>
2024-10-28 15:26:30 +0530

    theme color set, carousel slide animation

--- public/index.js
+++ public/index.js
@@ -22,47 +22,44 @@ document.addEventListener('DOMContentLoaded', function() {
   let currentSlide = 0;
   const slides = document.querySelectorAll('.carousel-item');
   const totalSlides = slides.length;
-  
+  const carouselContainer = document.querySelector('.carousel-container');
+
   const showSlide = (index) => {
-    slides.forEach((slide, i) => {
-      slide.style.display = i === index ? 'block' : 'none';
-    });
+    const offset = -index * 100; // Calculate the offset for the slide
+    carouselContainer.style.transform = `translateX(${offset}%)`;
   };
 
   const nextSlide = () => {
     currentSlide = (currentSlide + 1) % totalSlides;
     showSlide(currentSlide);
   };
-  
+
   showSlide(currentSlide); // Initialize with the first slide
   setInterval(nextSlide, 3000); // Change slide every 3 seconds
 });
 
 
+// document.addEventListener('DOMContentLoaded', function() {
+//   let currentSlide = 0;
+//   const slides = document.querySelectorAll('.carousel-item');
+//   const totalSlides = slides.length;
+  
+//   const showSlide = (index) => {
+//     slides.forEach((slide, i) => {
+//       slide.style.display = i === index ? 'block' : 'none';
+//     });
+//   };
+
+//   const nextSlide = () => {
+//     currentSlide = (currentSlide + 1) % totalSlides;
+//     showSlide(currentSlide);
+//   };
+  
+//   showSlide(currentSlide); // Initialize with the first slide
+//   setInterval(nextSlide, 3000); // Change slide every 3 seconds
+// });
 
 //text animation
-// document.addEventListener('DOMContentLoaded', () => {
-//   let splitTextClasses = [".split-1", ".split-2", ".split-3", ".split-4", ".split-5", ".split-6", ".split-7"];
-
-// splitTextClasses.forEach((splitClass, index) => {
-//   let mySplitText = new SplitType(splitClass, {type: "chars"});
-//   let chars = mySplitText.chars;
-
-//   //GSAP animation with ScrollTrigger
-//   gsap.from(chars, {
-//   xPercent: -100,
-//   opacity: 0,
-//  stagger: 0.1,
-//  ease:"power2.out",
-//  duration:0.3,
-//  scrollTrigger: {
-//   trigger: splitClass,
-//   start: "top 80%",
- 
-//  }
-// });
-// });
-// });
 
 document.addEventListener('DOMContentLoaded', () => {
   // Define the elements to animate
